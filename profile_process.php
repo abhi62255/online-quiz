062a6b124e43e368107b40abf90a1c92583edf47
abhi62255 <abhi62255@gmail.com>
2017-09-01 08:36:02 +0530

    JOB application form

---
+++ profile_process.php
@@ -0,0 +1,39 @@
+<?php
+if(isset($_POST["submit"]))
+{
+	if($FILES["resume"]["type"]=="txt/plain")
+	{
+		move_uploaded_file($_FILES["resume"]["tmp_name"],C:\Users\Abhishek Sharma\Documents\fashion blog)
+		$name=$_POST["name"];
+		$age=$_POST["age"];
+		$email=$_POST["email"];
+		$contact=$_POST["contact"];
+		$registration_number=rand(1000000,100000000)
+	}
+?>
+	
+	<html>
+<head>
+<title>resume</title>
+</head>
+<body>
+	<pre>
+	<table border="0" cellpadding="5">
+		<h2>Job Profile Registration</h2>
+		<tr><th>Name</th><td><?php echo $name ?></td></tr>
+		<tr><th>Age</th><td><?php echo $age ?></td></tr>    
+		<tr><th>Email</th><td><?php echo $email ?></td></tr>
+		<tr><th>Contact</th><td><?php echo $contact ?></td></tr>
+		<tr><th>Registration Number</th><td><?php echo $registration_number ?></td></tr>
+		<tr><td colspan="2" align="right"><input type="submit" name="submit" value="submit"></td></tr>
+		
+	</form>
+	</pre>
+
+</body>
+</html>
+
+
+<?php
+}
+?>
